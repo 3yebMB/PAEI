40d41b526a56dc3750aaa78b35643b8321aa7dcf
3yebmb <3yebmb@gmail.com>
2019-01-03 21:42:41 +0500

    1

--- src/Coworker.java
+++ src/Coworker.java
@@ -1,2 +1,10 @@
 public class Coworker {
+    private String name = "Unknown";
+    private String office = "Unknown";
+    private String mail = "Unknown";
+    private String phone = "Unknown";
+    private float salary = 0.0f;
+    private int age = 0;
+
+
 }
